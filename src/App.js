1a6712e6704469a6bd4a9179786a2441d974153b
ahsankhan201010 <ahsankhan201010@gmail.com>
2022-05-31 00:20:42 +0500

    demo test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import GridLayout from "react-grid-layout";
+import "/node_modules/react-grid-layout/css/styles.css"
+import "/node_modules/react-resizable/css/styles.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GridLayout style={{background: "lightgreen"}} isBounded={true}  compactType='horizontal' className="layout" cols={6} rowHeight={60} width={1200} >
+        <div style={{background: "lightgray"}} key="a" data-grid={{ x: 0, y: 0, w: 1, h: 2, maxH: 2 }}>
+          a
+        </div>
+        <div style={{background: "lightgray"}} key="b" data-grid={{ x: 1, y: 0, w: 1, h: 1, maxH: 2 }}>
+          b
+        </div>
+        <div style={{background: "lightgray"}} key="c" data-grid={{ x: 2, y: 0, w: 1, h: 1, maxH: 2  }}>
+          c
+        </div>
+      </GridLayout>
   );
 }
 
